3e6413132dedaf18b1011fe1c0d06e380a86ea32
saepulfariz <saepulfariz108@gmail.com>
2024-06-16 10:41:58 +0700

    add migration table and setup sqlite3

---
+++ app/Database/Migrations/2024-06-16-032815_Lamps.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Lamps extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'status' => [
+                'type'       => 'INT',
+                'default' => 0
+            ],
+            'created_at' => [
+                'type'           => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type'           => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('lamps');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('lamps');
+    }
+}
---
+++ app/Database/Migrations/2024-06-16-033209_Histories.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Histories extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'lamp_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'status' => [
+                'type'       => 'INT',
+                'default' => 0
+            ],
+            'created_at' => [
+                'type'           => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type'           => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('lamp_id', 'lamps', 'id', 'NULL', "CASCADE");
+        $this->forge->createTable('histories');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('histories');
+    }
+}
